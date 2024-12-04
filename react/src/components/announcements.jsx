c0b184c5c75b5ba08d4ecf6a6bd3e7c5071ad358
elprofesorsunny <elprofesorsunny@gmail.com>
2024-12-04 10:57:13 +0330

    Implement Announcements component to display dynamic content from JSON
    - Added Announcements component to fetch data from `announcementsData.json`.
    - Used `useEffect` and `useState` hooks to load and display announcements dynamically.
    - Styled announcement items with Tailwind CSS for a consistent layout.
    - Integrated announcements with links to display additional information.

---
+++ react/src/components/announcements.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import announcementsData from '../data/announcementsData.json';
+
+const Announcements = () => {
+  const [announcements, setAnnouncements] = useState([]);
+
+  useEffect(() => {
+    setAnnouncements(announcementsData);
+  }, []);
+
+  return (
+    <main className="w-[60vw] mx-auto py-4">
+      <div className="grid grid-cols-4 gap-4 section1">
+        <aside className="flex flex-col bg-[#fcfaf3] shadow-md rounded-lg p-4 justify-center">
+          {announcements.map((announcement, index) => (
+            <article key={index} className="p-4">
+              <a href={announcement.link} className="text-lg font-bold text-[#007773] hover:underline flex pb-4">
+                {announcement.title}
+              </a>
+              <p className="text-sm text-gray-600">{announcement.description}</p>
+              <a href={announcement.link} className="text-xs text-[#007773] hover:underline flex pt-4">
+                ادامه خبر
+              </a>
+            </article>
+          ))}
+        </aside>
+        <section className="col-span-3 h-[24vw] bg-gray-200 rounded-lg overflow-hidden shadow-md mr-2">
+          <img src="../src/assets/imgs/slider-1.jpg" alt="slider-1" className="h-full w-full object-fill" />
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Announcements;
