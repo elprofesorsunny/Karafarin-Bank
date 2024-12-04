11833eb4dc0cc9f2de42a82dc6ad9e1de55ffd72
elprofesorsunny <elprofesorsunny@gmail.com>
2024-12-04 10:56:06 +0330

    Set up basic layout with Header and Announcements components in App.jsx
    - Imported and rendered Header and Announcements components in the main App component.
    - Initialized the `useState` hook for managing state (currently unused).

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header'
+import Announcements from './components/announcements'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     
-    <div></div>
+    <div>
+      <Header></Header>
+      <Announcements></Announcements>
+    </div>
 
   )
 }
