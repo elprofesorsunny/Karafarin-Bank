dc898564d1beabf670d0a2be63ee11841c26580a
elprofesorsunny <elprofesorsunny@gmail.com>
2024-12-04 11:28:09 +0330

    Import and integrate Carousel component into App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
 import Announcements from './components/announcements'
+import Carousel from './components/carousel'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <div>
       <Header></Header>
       <Announcements></Announcements>
+      <Carousel></Carousel>
     </div>
 
   )
