0f25e527bfb27444a717bd1044b2e46ef4c18e65
elprofesorsunny <elprofesorsunny@gmail.com>
2024-12-07 11:37:01 +0330

    feat: Add PostFooterSection component with dynamic content
    - Created PostFooterSection component to display items from postFooter.json.
    - Included two sections: circular icons and bank/platform details.
    - Styled the sections with flexbox and custom background colors for visual separation.
    - Used dynamic data mapping for items in both sections.

---
+++ react/src/components/postFooter.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import sectionData from "../data/postFooter.json";
+
+const PostFooterSection = () => {
+  return (
+    <div className="bg-[#FCFAF3] py-4">
+      <div className="max-w-6xl mx-auto px-4 pb-8">
+        <div className="grid grid-cols-2 gap-8">
+          <div className="flex justify-start items-center gap-4">
+            {sectionData.items1.map((item, index) => (
+              <div key={index}>
+                <span
+                  className="block bg-gray-400 rounded-full"
+                  style={{ width: item.width, height: item.height }}
+                ></span>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex justify-end items-center space-x-8 gap-3">
+            {sectionData.items2.map((item, index) => (
+              <div
+                key={index}
+                className="bg-gray-900 rounded-lg p-4 text-white flex items-center gap-3 w-50"
+              >
+                <span className="bg-gray-400 rounded-full w-6 h-6"></span>
+                <div>
+                  <div>{`ویژه کاربران ${item.platform}`}</div>
+                  <div>{item.bank}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostFooterSection;
