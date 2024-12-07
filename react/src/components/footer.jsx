15e5b8f8b0daf6f9194f2dee8420eb74d2d9598a
elprofesorsunny <elprofesorsunny@gmail.com>
2024-12-07 11:31:44 +0330

    feat: Create responsive footer with dynamic content from JSON
    - Added Footer component to display dynamic sections from footerData.json.
    - Used grid layout to organize footer items into four columns.
    - Styled section titles with color and spacing for improved readability.
    - Rendered each footer section dynamically based on the provided data.
    - Implemented list items with custom bullet points for footer links.

---
+++ react/src/components/footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import footerData from "../data/footerData.json";
+
+const Footer = () => {
+  return (
+    <div className="back-section5 bg-[#FCFAF3]">
+      <div className="section5 max-w-[60vw] mx-auto py-8">
+        <div className="grid-section5-items grid grid-cols-4 gap-4">
+          {footerData.map((section, index) => (
+            <div key={index} className={`section5-item${index + 1}`}>
+              <div className="section5-titles text-[#007773] pb-4">
+                {section.title}
+              </div>
+              <ul className="list-none">
+                {section.items.map((item, i) => (
+                  <li key={i} className="flex gap-2">
+                    <span className="square">◉</span> {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
