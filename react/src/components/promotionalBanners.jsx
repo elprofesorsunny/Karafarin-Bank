3ee6d7d10aa1c412dd85b21386ea5aff6da6d31d
elprofesorsunny <elprofesorsunny@gmail.com>
2024-12-07 08:48:39 +0330

    Add PromotionalBanners component to display promotional banners
    - Renders promotional banners dynamically from bannersData.json.
    - Uses a responsive grid layout with two columns and two rows for banner display.
    - Applies custom background image and styling for the banner section.
    - Ensures images are displayed in a fully covered, responsive manner with rounded corners.

---
+++ react/src/components/promotionalBanners.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import bannersData from "../data/bannersData.json";
+
+const PromotionalBanners = () => {
+  return (
+    <div
+      id="back-section4"
+      className="bg-cover bg-center pb-12"
+      style={{ backgroundImage: "url(../assets/imgs/background-bottom.png)" }}
+    >
+      <div className="section4 max-w-[60vw] mx-auto py-4">
+        <div
+          id="grid-section4-banners"
+          className="grid grid-cols-2 grid-rows-2 gap-8"
+        >
+          {bannersData.map((banner) => (
+            <div
+              key={banner.id}
+              className={`${banner.className} bg-[#FCFAF3] shadow-lg rounded-lg h-[20rem] overflow-hidden`}
+            >
+              <img
+                src={banner.image}
+                alt={`Banner ${banner.id}`}
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PromotionalBanners;
