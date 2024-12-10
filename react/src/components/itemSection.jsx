9c5f0868bffdcc19e489bb4ee65ff0060df0f29f
elprofesorsunny <elprofesorsunny@gmail.com>
2024-12-10 09:46:08 +0330

    feat: create ItemsSection component to manage the sidebar items

---
+++ react/src/components/itemSection.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ItemsSection = ({ sidebarData }) => {
+  return (
+    <div className="section3-items flex items-center">
+      <div className="grid grid-rows-5 rounded-2xl mx-6 gap-4 w-full">
+        {sidebarData.map((item, index) => (
+          <a
+            key={index}
+            href={item.link}
+            className="sidebar-item flex items-center gap-4 p-2 bg-[#FCFAF3] rounded-lg shadow-lg transition-all hover:bg-[#c1c7ad]"
+          >
+            <span className="text-xl text-gray-600">{item.icon}</span>
+            <span className="text-sm text-gray-800">{item.text}</span>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ItemsSection;
